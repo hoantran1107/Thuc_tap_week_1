272e50772d0183cbf470d35edf750a78e2bf441f
hoantran1107 <hoantran1107@gmail.com>
2022-11-23 16:04:24 +0700

    23/11

--- Thuc_Tap_week_1/Thuc_Tap_week_1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Thuc_Tap_week_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

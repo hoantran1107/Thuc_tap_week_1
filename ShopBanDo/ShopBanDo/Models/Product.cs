5d7192b1083008266ea3d693eae9a3e4fcd1b503
hoantran1107 <hoantran1107@gmail.com>
2022-12-01 20:25:51 +0700

    Update CSDL-Pageadmin

---
+++ ShopBanDo/ShopBanDo/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ShopBanDo.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ShortDesc { get; set; }
+        public string Description { get; set; }
+        public int? CatId { get; set; }
+        public int? Price { get; set; }
+        public int? Discount { get; set; }
+        public string Thumb { get; set; }
+        public string Video { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+        public bool BestSeller { get; set; }
+        public bool HomeFlag { get; set; }
+        public bool Active { get; set; }
+        public string Tags { get; set; }
+        public string Alias { get; set; }
+        public string MetaDesc { get; set; }
+        public string MetaKey { get; set; }
+        public int? UnitslnStock { get; set; }
+
+        public virtual Category Cat { get; set; }
+    }
+}
---
+++ ShopBanDo/ShopBanDo/Models/TinTuc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ShopBanDo.Models
+{
+    public partial class TinTuc
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Scontents { get; set; }
+        public string Contents { get; set; }
+        public string Thumb { get; set; }
+        public string Alias { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public bool Published { get; set; }
+        public string Author { get; set; }
+        public int? AccountId { get; set; }
+        public string Tags { get; set; }
+        public int? CatId { get; set; }
+        public bool? IsHot { get; set; }
+        public bool? IsNewfeed { get; set; }
+        public string MetaKey { get; set; }
+        public string MetaDesc { get; set; }
+        public int? Views { get; set; }
+    }
+}

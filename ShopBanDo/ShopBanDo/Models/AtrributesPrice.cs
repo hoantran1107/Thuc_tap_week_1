52c382dde984d98852a8f09bcc8afe66cd3b7c2e
hoantran1107 <hoantran1107@gmail.com>
2022-12-01 21:04:33 +0700

    update_csdl

---
+++ ShopBanDo/ShopBanDo/Models/AtrributesPrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ShopBanDo.Models
+{
+    public partial class AtrributesPrice
+    {
+        public int AttributesPriceId { get; set; }
+        public int? AttributeId { get; set; }
+        public int? ProductId { get; set; }
+        public int? Price { get; set; }
+        public bool Active { get; set; }
+    }
+}

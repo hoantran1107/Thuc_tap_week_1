644b9a93b5f5e3ef7c10d7214e2f56cab902f62f
hoantran1107 <hoantran1107@gmail.com>
2022-12-08 10:07:38 +0700

    them

--- ShopBanDo/ShopBanDo/Controllers/HomeController.cs
+++ ShopBanDo/ShopBanDo/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ShopBanDo.Models;
+using ShopBanDo.ModelView;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,19 +15,71 @@ namespace ShopBanDo.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly dbshopContext _context;
+        public HomeController(ILogger<HomeController> logger,dbshopContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
-        } 
+            //tao ra instance homeView gom List<ProductHomeVM>,List<TinTuc>,1 page quang cao
+            //model chinh
+            HomeViewVM model = new HomeViewVM();
+            //lay ra san pham duoc xuat hien o trang home
+            var lsProducts = _context.Products.AsNoTracking()
+                .Where(x => x.Active == true && x.HomeFlag == true)
+                .OrderByDescending(x => x.DateCreated)
+                .ToList();
+            //tao ra 1 list productHomeVM
+            List<ProductHomeVM> lsProductViews = new List<ProductHomeVM>();
+            //lay ra cac danh muc san pham
+            var lsCats = _context.Categories
+                .AsNoTracking()
+                .Where(x => x.Published == true)
+                .OrderByDescending(x => x.Ordering)
+                .ToList();
+            //duyet cac sanh mục
+            foreach (var item in lsCats)
+            {
+                //ProductHomeVM co danh muc va cac san pham thuoc danh muc
+                ProductHomeVM productHome = new ProductHomeVM();
+                //moi cai productHome co thuoc tinh category la danh muc
+                productHome.category = item;
+                //1 list can pham thuc danh muc do
+                productHome.lsProducts = lsProducts.Where(x => x.CatId == item.CatId).ToList();
+                //dua cai producthome vao list
+                lsProductViews.Add(productHome);
+                //lay ra page quang cao
+                var quangcao = _context.Pages
+                    .AsNoTracking()
+                    .FirstOrDefault(x => x.Published == true);
+                //lay ra 3 tin tuc
+                var TinTuc = _context.TinTucs
+                    .AsNoTracking()
+                    .Where(x => x.Published == true && x.IsNewfeed == true)
+                    .OrderByDescending(x => x.CreatedDate)
+                    .Take(3)
+                    .ToList();
+                //1 la list gom (list<product>,category cua cac san pham do
+                //model.Products= {{ao,ao thun,ao dai tay}, men },{{quan,quan dui},girl}
+                model.Products = lsProductViews;
+                //lay ra 1 page quang cao
+                model.Page = quangcao;
+                //3 tin tuc o la isnewfeed
+                model.TinTucs = TinTuc;
+                //tat ca san pham duoc ghim o trang home
+                ViewBag.AllProducts = lsProducts;
+            }
+            return View(model);
+        }
+        [Route("/lien-he.html", Name = "Contact")]
         public IActionResult Contact()
         {
             return View();
-        } 
+        }
+        [Route("/gioi-thieu.html", Name = "About")]
         public IActionResult About()
         {
             return View();
---
+++ ShopBanDo/ShopBanDo/Controllers/PagesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopBanDo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBanDo.Controllers
+{
+    public class PagesController : Controller
+    {
+        private readonly dbshopContext _context;
+        public PagesController(dbshopContext context)
+        {
+            _context = context;
+        }
+        //Get: page/Alias
+        [Route("/page/{Alias}", Name = "PageDetails")]
+        public async Task<IActionResult> Details(string Alias)
+        {
+            if (string.IsNullOrEmpty(Alias))
+            {
+                return RedirectToAction("Index","Home");
+            }
+
+            var page = await _context.Pages
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Alias == Alias);
+            if (page == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
+            return View(page);
+        }
+    }
+}

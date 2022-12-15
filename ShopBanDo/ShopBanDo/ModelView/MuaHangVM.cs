4aa676b7f790e654693bc521910e7828d921bd3c
Trần Hải Phước <93334312+tranhaiphuoc@users.noreply.github.com>
2022-12-15 21:25:05 +0700

    Change to english in ModelView

--- ShopBanDo/ShopBanDo/ModelView/MuaHangVM.cs
+++ ShopBanDo/ShopBanDo/ModelView/MuaHangVM.cs
@@ -10,18 +10,18 @@ namespace ShopBanDo.ModelView
     {
         public int CustomerId { get; set; }
 
-        [Required(ErrorMessage = "Vui lòng nhập Họ và Tên")]
+        [Required(ErrorMessage = "Enter your full name")]
         public string FullName { get; set; }
         public string Email { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        [Required(ErrorMessage = "Enter your phone number")]
         public string Phone { get; set; }
-        [Required(ErrorMessage = "Địa chỉ nhận hàng")]
+        [Required(ErrorMessage = "Enter your address")]
         public string Address { get; set; }
-        [Required(ErrorMessage = "Vui lòng chọn Tỉnh/Thành")]
+        [Required(ErrorMessage = "Select your province/city")]
         public int TinhThanh { get; set; }
-        [Required(ErrorMessage = "Vui lòng chọn Quận/Huyện")]
+        [Required(ErrorMessage = "Enter your district")]
         public int QuanHuyen { get; set; }
-        [Required(ErrorMessage = "Vui lòng chọn Phường/Xã")]
+        [Required(ErrorMessage = "Enter your ward/communes")]
         public int PhuongXa { get; set; }
         public int PaymentID { get; set; }
         public string Note { get; set; }
--- ShopBanDo/ShopBanDo/ModelView/RegisterViewModel.cs
+++ ShopBanDo/ShopBanDo/ModelView/RegisterViewModel.cs
@@ -12,12 +12,12 @@ namespace ShopBanDo.ModelView
         [Key]
         public int CustomerId { get; set; }
 
-        [Display(Name = "Họ và Tên")]
-        [Required(ErrorMessage = "Vui lòng nhập Họ Tên")]
+        [Display(Name = "Full name")]
+        [Required(ErrorMessage = "Enter your full name again")]
         public string FullName { get; set; }
 
         [MaxLength(150)]
-        [Required(ErrorMessage = "Vui lòng nhập Email")]
+        [Required(ErrorMessage = "Enter your email again")]
         [DataType(DataType.EmailAddress)]
         //send ajax request,tra ve kieu json voi action va controller
         //kiem tra email da ton tai
@@ -25,22 +25,22 @@ namespace ShopBanDo.ModelView
         public string Email { get; set; }
 
         [MaxLength(11)]
-        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
-        [Display(Name = "Điện thoại")]
+        [Required(ErrorMessage = "Enter your phone number")]
+        [Display(Name = "Phone number")]
         [DataType(DataType.PhoneNumber)]
         //kiem tra dien thoai da ton tai
         [Remote(action: "ValidatePhone", controller: "Accounts")]
         public string Phone { get; set; }
 
-        [Display(Name = "Mật khẩu")]
-        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
-        [MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Enter your password")]
+        [MinLength(5, ErrorMessage = "Password's length must be at least 5 characters")]
         public string Password { get; set; }
 
-        [MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
-        [Display(Name = "Nhập lại mật khẩu")]
+        [MinLength(5, ErrorMessage = "Password's length must be at least 5 characters")]
+        [Display(Name = "Enter your password again")]
         //so sanh pass
-        [Compare("Password", ErrorMessage = "Nhập lại mật khẩu không đúng")]
+        [Compare("Password", ErrorMessage = "Password is not correct")]
         public string ConfirmPassword { get; set; }
     }
 }

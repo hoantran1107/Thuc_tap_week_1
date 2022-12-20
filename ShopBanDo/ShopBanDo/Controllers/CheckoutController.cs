ebe015c40ba1d58f28f64e6c6d8243e7ce56b6ad
hoantran1107 <hoantran1107@gmail.com>
2022-12-20 19:33:15 +0700

    update mua hang

--- ShopBanDo/ShopBanDo/Controllers/CheckoutController.cs
+++ ShopBanDo/ShopBanDo/Controllers/CheckoutController.cs
@@ -1,31 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AspNetCoreHero.ToastNotification.Abstractions;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using ShopBanDo.Extension;
-using ShopBanDo.Helpper;
-using ShopBanDo.Models;
-using ShopBanDo.ModelView;
+﻿namespace WebShop.Controllers
+{
+    using AspNetCoreHero.ToastNotification.Abstractions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using ShopBanDo.Extension;
+    using ShopBanDo.Helpper;
+    using ShopBanDo.Models;
+    using ShopBanDo.ModelView;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace WebShop.Controllers
-{   
     public class CheckoutController : Controller
     {
-
         private readonly dbshopContext _context;
+
         public INotyfService _notyfService { get; }
+
         public CheckoutController(dbshopContext context, INotyfService notyfService)
         {
             _context = context;
             _notyfService = notyfService;
         }
+
         public List<CartItem> GioHang
         {
             get
@@ -38,24 +36,27 @@ namespace WebShop.Controllers
                 return gh;
             }
         }
+
         [Route("checkout.html", Name = "Checkout")]
         public IActionResult Index(string returnUrl = null)
         {
-            //Lay gio hang ra de xu ly
             var cart = HttpContext.Session.Get<List<CartItem>>("GioHang");
             var taikhoanID = HttpContext.Session.GetString("CustomerId");
             MuaHangVM model = new MuaHangVM();
-            if (taikhoanID != null)
+            if (taikhoanID == null)
             {
-                var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.CustomerId == Convert.ToInt32(taikhoanID));
-                model.CustomerId = khachhang.CustomerId;
-                model.FullName = khachhang.FullName;
-                model.Email = khachhang.Email;
-                model.Phone = khachhang.Phone;
-                model.Address = khachhang.Address;
+                ViewBag.GioHang = cart;
+                return View(model);
             }
-            //day model sang
-         /*   ViewData["lsTinhThanh"] = new SelectList(_context.Locations.Where(x => x.Levels == 1).OrderBy(x => x.Type).ToList(), "Location", "Name");*/
+
+            var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.CustomerId == Convert.ToInt32(taikhoanID));
+            model.CustomerId = khachhang.CustomerId;
+            model.FullName = khachhang.FullName;
+            model.Email = khachhang.Email;
+            model.Phone = khachhang.Phone;
+            model.Address = khachhang.Address;
+
+            /*   ViewData["lsTinhThanh"] = new SelectList(_context.Locations.Where(x => x.Levels == 1).OrderBy(x => x.Type).ToList(), "Location", "Name");*/
             ViewBag.GioHang = cart;
             return View(model);
         }
@@ -64,97 +65,90 @@ namespace WebShop.Controllers
         [Route("checkout.html", Name = "Checkout")]
         public IActionResult Index(MuaHangVM muaHang)
         {
-            //Lay ra gio hang de xu ly
             var cart = HttpContext.Session.Get<List<CartItem>>("GioHang");
             var taikhoanID = HttpContext.Session.GetString("CustomerId");
             MuaHangVM model = new MuaHangVM();
-            if (taikhoanID != null)
-            {
-                var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.CustomerId == Convert.ToInt32(taikhoanID));
-                model.CustomerId = khachhang.CustomerId;
-                model.FullName = khachhang.FullName;
-                model.Email = khachhang.Email;
-                model.Phone = khachhang.Phone;
-                model.Address = khachhang.Address;
-                khachhang.Address = muaHang.Address;
-                _context.Update(khachhang);
-                _context.SaveChanges();
-            }
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    //Khoi tao don hang
-                    Order donhang = new Order();
-                    donhang.CustomerId = model.CustomerId;
-                    donhang.Address = model.Address;
-                    donhang.OrderDate = DateTime.Now;
-                    donhang.TransactStatusId = 1;//Don hang moi
-                    donhang.Deleted = false;
-                    donhang.Paid = false;
-                    donhang.Note = Utilities.StripHTML(model.Note);
-                    donhang.Total = Convert.ToInt32(cart.Sum(x => x.TotalMoney));
-                    _context.Add(donhang);
-                    _context.SaveChanges();
-                    //tao ordertail
-                    foreach (var item in cart)
-                    {
-                        OrderDetail orderDetail = new OrderDetail();
-                        orderDetail.OrderId = donhang.OrderId;
-                        orderDetail.ProductId = item.product.ProductId;
-                        orderDetail.Quantity = item.amount;
-                        orderDetail.Total = item.amount * item.product.Price;
-                        orderDetail.Price = item.product.Price;
-                        orderDetail.CreateDate = DateTime.Now;
-                        _context.Add(orderDetail);
-                    }
-                    _context.SaveChanges();
-                    //clear gio hang
-                    HttpContext.Session.Remove("GioHang");
-                    //Xuat thong bao
-                    _notyfService.Success("Đơn hàng đặt thành công");
-                    //cap nhat thong tin khach hang
-                    return RedirectToAction("Success");
 
-
-                }
-            }
-            catch
+            if (taikhoanID == null)
             {
-                /*ViewData["lsTinhThanh"] = new SelectList(_context.Locations.Where(x => x.Levels == 1).OrderBy(x => x.Type).ToList(), "Location", "Name");*/
                 ViewBag.GioHang = cart;
                 return View(model);
             }
-            /*ViewData["lsTinhThanh"] = new SelectList(_context.Locations.Where(x => x.Levels == 1).OrderBy(x => x.Type).ToList(), "Location", "Name");*/
-            ViewBag.GioHang = cart;
-            return View(model);
-        }
-        [Route("dat-hang-thanh-cong.html", Name = "Success")]
-        public IActionResult Success()
-        {
+
+            var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.CustomerId == Convert.ToInt32(taikhoanID));
+            model.CustomerId = khachhang.CustomerId;
+            model.FullName = khachhang.FullName;
+            model.Email = khachhang.Email;
+            model.Phone = khachhang.Phone;
+            model.Address = khachhang.Address;
+            khachhang.Address = muaHang.Address;
+            _context.Update(khachhang);
+            _context.SaveChanges();
+
             try
             {
-                var taikhoanID = HttpContext.Session.GetString("CustomerId");
-                if (string.IsNullOrEmpty(taikhoanID))
+                if (!ModelState.IsValid)
                 {
-                    return RedirectToAction("Login", "Accounts", new { returnUrl = "/dat-hang-thanh-cong.html" });
+                    ViewBag.GioHang = cart;
+                    return View(model);
                 }
-                var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.CustomerId == Convert.ToInt32(taikhoanID));
-                var donhang = _context.Orders
-                    .Where(x => x.CustomerId == Convert.ToInt32(taikhoanID))
-                    .OrderByDescending(x => x.OrderDate)
-                    .FirstOrDefault();
-                MuaHangSuccessVM successVM = new MuaHangSuccessVM();
-                successVM.FullName = khachhang.FullName;
-                successVM.DonHangID = donhang.OrderId;
-                successVM.Phone = khachhang.Phone;
-                successVM.Address = khachhang.Address;
-                return View(successVM);
+
+                Order donhang = new Order();
+                donhang.CustomerId = model.CustomerId;
+                donhang.Address = model.Address;
+                donhang.OrderDate = DateTime.Now;
+                donhang.TransactStatusId = 1;//Don hang moi
+                donhang.Deleted = false;
+                donhang.Paid = false;
+                donhang.Note = Utilities.StripHTML(model.Note);
+                donhang.Total = Convert.ToInt32(cart.Sum(x => x.TotalMoney));
+                _context.Add(donhang);
+                _context.SaveChanges();
+
+                foreach (var item in cart)
+                {
+                    OrderDetail orderDetail = new OrderDetail();
+                    orderDetail.OrderId = donhang.OrderId;
+                    orderDetail.ProductId = item.product.ProductId;
+                    orderDetail.Quantity = item.amount;
+                    orderDetail.Total = item.amount * item.product.Price;
+                    orderDetail.Price = item.product.Price;
+                    orderDetail.CreateDate = DateTime.Now;
+                    _context.Add(orderDetail);
+                }
+                _context.SaveChanges();
+
+                HttpContext.Session.Remove("GioHang");
+                _notyfService.Success("Checkout success");
+                return RedirectToAction("Success");
             }
             catch
             {
-                return View();
+                ViewBag.GioHang = cart;
+                return View(model);
             }
         }
+
+        [Route("dat-hang-thanh-cong.html", Name = "Success")]
+        public IActionResult Success()
+        {
+
+            var taikhoanID = HttpContext.Session.GetString("CustomerId");
+            if (string.IsNullOrEmpty(taikhoanID))
+            {
+                return RedirectToAction("Login", "Accounts", new { returnUrl = "/dat-hang-thanh-cong.html" });
+            }
+            var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.CustomerId == Convert.ToInt32(taikhoanID));
+            var donhang = _context.Orders
+                .Where(x => x.CustomerId == Convert.ToInt32(taikhoanID))
+                .OrderByDescending(x => x.OrderDate)
+                .FirstOrDefault();
+            MuaHangSuccessVM successVM = new MuaHangSuccessVM();
+            successVM.FullName = khachhang.FullName;
+            successVM.DonHangID = donhang.OrderId;
+            successVM.Phone = khachhang.Phone;
+            successVM.Address = khachhang.Address;
+            return View(successVM);
+        }
     }
-    }
+}

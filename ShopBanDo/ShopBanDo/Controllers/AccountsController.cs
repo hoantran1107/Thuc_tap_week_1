3a0e2c10bdc8f1286c154a54ad190ac5c3b3895b
Trần Khải Hoàn <105619991+hoantran1107@users.noreply.github.com>
2022-12-27 19:49:37 +0700

    update

--- ShopBanDo/ShopBanDo/Controllers/AccountsController.cs
+++ ShopBanDo/ShopBanDo/Controllers/AccountsController.cs
@@ -331,7 +331,7 @@
             return View();
         }
         [HttpPost]
-        [AllowAnonymous]
+        [AuthoriteFilter]
         [Route("/resetpassword/{Email}", Name = ("ResetPassword"))]
         public IActionResult ResetPassword(ResetPasswordModelView model, string email)
         {
@@ -341,6 +341,7 @@
             taikhoan.Password = passnew;
 
             _customer.UpdatePass(taikhoan, true);
+            HttpContext.Session.SetString("CustomerId", taikhoan.CustomerId.ToString());
             _notyfService.Success("Change password success");
             return RedirectToAction("Dashboard");
         }
